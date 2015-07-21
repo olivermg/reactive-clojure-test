17ecf21a8154fcf951ad4f2aaf7c585e4724e709
Oliver Wegner <void1976@gmail.com>
2015-07-21 16:20:49 +0200

    more bootstrap buttons

--- src/cljs/manager_web/core.cljs
+++ src/cljs/manager_web/core.cljs
@@ -2,19 +2,29 @@
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
             [om-bootstrap.button :as btn]
-            [om-bootstrap.random :as rnd]))
+            [om-bootstrap.random :as rnd]
+            [om-bootstrap.grid :as grid]))
 
 (enable-console-print!)
 
 (defonce app-state (atom {:text "Hello Chestnut!"}))
 
+(defn buttons []
+  (dom/div nil
+           (btn/toolbar
+            {}
+            (btn/button {} "Button 1")
+            (btn/button {:bs-style "primary"} "Primary")
+            (btn/button {:bs-style "success"} "Success"))))
+
 (defn main []
+
   (om/root
     (fn [app owner]
       (reify
         om/IRender
         (render [_]
-          (dom/h1 nil (:text app)))))
+          (rnd/page-header {} "Manager console"))))
     app-state
     {:target (. js/document (getElementById "app"))})
 
@@ -23,11 +33,9 @@
      (reify
        om/IRender
        (render [_]
-         (dom/div nil
-                  (btn/toolbar
-                   {}
-                   (btn/button {} "Button 1")
-                   (btn/button {:bs-style "primary"} "Primary")
-                   (btn/button {:bs-style "success"} "Success"))))))
+         (grid/grid {}
+                    (grid/row {}
+                              (grid/col {:xs 12 :md 8} (buttons))
+                              (grid/col {:xs 6 :md 4} "col 2"))))))
    app-state
-   {:target (. js/document (getElementById "row1"))}))
+   {:target (. js/document (getElementById "container"))}))
